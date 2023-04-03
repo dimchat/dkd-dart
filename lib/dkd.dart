9c1b884f04b9decaf2bea1a84dd23ea29eec98a5
Moky <albert.moky@gmail.com>
2023-04-03 23:15:03 +0800

    proj init

---
+++ lib/dkd.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dkd;
+
+export 'src/dkd_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
